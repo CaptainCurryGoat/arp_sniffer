dc88e91079335c1cf50b0d200cb4befa25c3f5c3
Daniel Edwards <dannygithub@gmail.com>
2017-05-09 09:59:21 -0400

    created passive_sniffer..performs the acutal sniffing

--- arp_sniffer/__init__.py
+++ arp_sniffer/__init__.py
@@ -28,4 +28,43 @@ class ArpEnumerator(object):
         self.filter = 'arp'
         self.starttime = datetime.now()
 
-        
+"""
+Checks if a host has been discovered.
+If not, print the IP and MAC.
+"""
+def passive_handler(self, pkt):
+
+    try:
+        if not pkt['ARP'].psrc in self.discovered_hosts.keys():
+
+                print(pkt['ARP'].prsc, pkt['ARP'].hwsrc)
+
+                self.discovered_hosts[pkt['ARP'].prsc] = pkt['ARP'].hwsrc
+
+    except KeyboardInterrupt:
+
+        return
+
+
+def passive_sniffer(self):
+
+    if not self.range:
+
+        print("Unspecified range; Sniffing all ARP traffic")
+
+    else:
+
+        self.filter += "and (net %s)" %(self.range)
+
+    print("Sniffing started on " + self.interface)
+
+    try:
+        sniff(filter=self.filter, prn=self.passive_handler, store=0)
+
+    except Exception:
+
+        print("")
+
+        return
+
+
